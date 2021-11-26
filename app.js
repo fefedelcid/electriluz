63189be55bfdee84606d7666a31ae4bfaa7d5317
fefedelcid <thegreench1996@gmail.com>
2021-11-26 20:18:00 -0300

    Primer commit de la app nodejs.

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+
+// Motor de plantillas
+app.set('views', __dirname + '/views'); // especifico el directorio de las plantillas
+app.set('view engine', 'ejs'); // registro el motor de plantillas
+
+app.use(express.static(__dirname + '/public')); // configuro archivos estaticos
+
+
+// ================================
+// Páginas
+// ================================
+
+app.get('/', (req, res)=>{
+	res.render('index')
+});
+
+app.use((req, res, next)=>{
+	res.status(404).render('404')
+})
+
+
+
+app.listen(port, () => {
+	console.log(__dirname)
+  console.log(`App listening at http://localhost:${port}`)
+});
